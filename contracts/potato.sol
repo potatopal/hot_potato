8397a1ad265bc90bf58ebbae7d9a9e33d1e342ed
squeaky1273 <pjriddell22@gmail.com>
2020-10-05 15:04:49 -0700

    added uint 256 and credits

--- contracts/potato.sol
+++ contracts/potato.sol
@@ -4,32 +4,37 @@ pragma solidity ^0.6.0;
 
 import "./SafeMath.sol";
 
-contract Potato is ERC721{
-	mapping(address => uint) tokens;
-	function approval(address _owner, address _approved,uint _tokenId){
-		require(tokens[_owner]==_tokenId);
-		tokens[_approved]=_tokenId;
+// Starter code from: https://github.com/alisyakainth/ERC721/blob/master/contract721.sol
+
+contract Potato is ERC721 {
+    
+    using SafeMath for uint256;
+
+	mapping(address => uint256) tokens;
+	function approval(address _owner, address _approved, uint256 _tokenId){
+		require(tokens[_owner] == _tokenId);
+		tokens[_approved] = _tokenId;
 	}
 	function transfer(address _to, uint _amount) public payable{
 		require(_amount <= tokens[msg.sender]);
-		tokens[msg.sender]-=_amount;
-		tokens[_to]+=_amount;
+		tokens[msg.sender] -= _amount;
+		tokens[_to] += _amount;
 	}
-	function balanceOf(address _owner) public view returns (uint){
-		return tokens[_owner];
+	function balanceOf(address _owner) public view returns (uint256){
+		return balances[uint256 _balance];
 	}
-	function ownerOf(uint _tokenId) public view returns(address){
+	function ownerOf(uint256 _tokenId) public view returns(address){
 		return tokens[_id].address;
 	}
-	function TransferFrom(address _from, address _to, uint _tokenId) payable{
-		require(tokens[_from]==_tokenId);
-		tokens[_from]=0;
-		tokens[_to]=_tokenId;
+	function TransferFrom(address _from, address _to, uint256 _tokenId) payable{
+		require(tokens[_from] == _tokenId);
+		tokens[_from] = 0;
+		tokens[_to] = _tokenId;
 	}
-	function approve(address _approved, uint _tokenId) payable{
-		require(tokens[msg.sender]==_tokenId);
-		tokens[_approved]=_tokenId;
+	function approve(address _approved, uint256 _tokenId) payable{
+		require(tokens[msg.sender] == _tokenId);
+		tokens[_approved] = _tokenId;
 	}
-	function mint(address _to, uint _tokenId,) public{
-		tokens[_to] = 'mytoken '+str(uint(blockhash(block.number - 1)));
-	}
+	// function mint(address _to, uint265 _tokenId,) public{
+	// 	tokens[_to] = 'mytoken '+str(uint(blockhash(block.number - 1)));
+	// }
