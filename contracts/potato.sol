0158dbfa2a0004b2b0c3d742d19c9c3f3aed9823
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-10-05 19:54:18 -0700

    Import basic potato contract

--- contracts/potato.sol
+++ contracts/potato.sol
@@ -1,40 +1,20 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-// SPDX-License-Identifier: MIT
+// Remix import syntax
+import "github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721Burnable.sol";
+import "github/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-import "./SafeMath.sol";
+contract Potato is ERC721, Ownable, ERC721Burnable {
+    constructor() ERC721("Potato", "HOT") public {}
 
-// Starter code from: https://github.com/alisyakainth/ERC721/blob/master/contract721.sol
+    function mint(address to, uint256 tokenId) public onlyOwner {
+        _mint(to, tokenId);
+    }
 
-contract Potato is ERC721 {
-    
-    using SafeMath for uint256;
+    function _mint(address to) public onlyOwner{
+        mint(to, totalSupply().add(1));
+    }
 
-	mapping(address => uint256) tokens;
-	function approval(address _owner, address _approved, uint256 _tokenId){
-		require(tokens[_owner] == _tokenId);
-		tokens[_approved] = _tokenId;
-	}
-	function transfer(address _to, uint _amount) public payable{
-		require(_amount <= tokens[msg.sender]);
-		tokens[msg.sender] -= _amount;
-		tokens[_to] += _amount;
-	}
-	function balanceOf(address _owner) public view returns (uint256){
-		return balances[uint256 _balance];
-	}
-	function ownerOf(uint256 _tokenId) public view returns(address){
-		return tokens[_id].address;
-	}
-	function TransferFrom(address _from, address _to, uint256 _tokenId) payable{
-		require(tokens[_from] == _tokenId);
-		tokens[_from] = 0;
-		tokens[_to] = _tokenId;
-	}
-	function approve(address _approved, uint256 _tokenId) payable{
-		require(tokens[msg.sender] == _tokenId);
-		tokens[_approved] = _tokenId;
-	}
-	// function mint(address _to, uint265 _tokenId,) public{
-	// 	tokens[_to] = 'mytoken '+str(uint(blockhash(block.number - 1)));
-	// }
+}
--- scripts/token.py
+++ scripts/token.py
@@ -5,4 +5,4 @@ from brownie import Token, accounts
 
 def main():
     accounts.load('metamask')
-    return Token.deploy("Test Token", "TST", 18, 1e21, {'from': accounts[0]})
+    return Token.deploy("Hot Potato", "HOT", 18, 1e21, {'from': accounts[0]})
